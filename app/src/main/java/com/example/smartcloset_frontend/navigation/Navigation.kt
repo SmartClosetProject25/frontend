4420bba552e37d68a9ef5a3a6becdccf6f4f7150
kazuuu552 <kazuuu559@gmail.com>
2025-10-31 11:51:24 +0900

    ダイアログテスト用の画面を追加
    - ダイアログ表示機能をHomeScreenからDialogTestScreenに移動
    - ナビゲーションに`dialogTest`ルートを追加

--- app/src/main/java/com/example/smartcloset_frontend/navigation/Navigation.kt
+++ app/src/main/java/com/example/smartcloset_frontend/navigation/Navigation.kt
@@ -4,12 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.smartcloset_frontend.ui.DialogTestScreen
 import com.example.smartcloset_frontend.ui.HomeScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
     NavHost(navController, startDestination = "home") {
         composable("home") { HomeScreen(navController) }
+        composable("dialogTest") { DialogTestScreen(navController) }
 //        composable("register") { RegisterScreen(navController) }
 //        composable("coordinate") { CoordinateScreen(navController) }
 //        composable("favorite") { FavoriteScreen(navController) }
