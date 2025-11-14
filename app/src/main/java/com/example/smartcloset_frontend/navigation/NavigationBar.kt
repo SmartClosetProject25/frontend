81a5c4d04eca54117b549ec7c0ea96428429e2b1
syxnji <syunji0710@gmail.com>
2025-11-14 11:50:46 +0900

    ナヴィゲーションを修正、アイコンを変更

--- app/src/main/java/com/example/smartcloset_frontend/navigation/NavigationBar.kt
+++ app/src/main/java/com/example/smartcloset_frontend/navigation/NavigationBar.kt
@@ -1,25 +1,46 @@
 package com.example.smartcloset_frontend.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.smartcloset_frontend.R
 
 @Composable
 fun BottomNavBar(navController: NavHostController) {
     NavigationBar {
-        val items = listOf("home", "coordinate", "register", "favorite", "settings", "test")
-        items.forEach { route ->
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        val items = listOf(
+            "home" to R.drawable.wardrobe_line,
+            "coordinate" to R.drawable.light_line,
+            // ItemCreateType?待ち。取り敢えずHOMEに飛ぶ
+            "home" to R.drawable.add_square_line,
+            "profile" to R.drawable.user_4_line,
+            "settings" to R.drawable.settings_4_line,
+            // 不要になったら消す(コメントアウトでも可)
+            "test" to R.drawable.bug_line
+        )
+
+        items.forEach { (route, iconRes) ->
             NavigationBarItem(
-                selected = false,
+                selected = currentRoute == route,
                 onClick = {
                     navController.navigate(route) {
                         launchSingleTop = true
                     }
                 },
-                label = { Text(route.replaceFirstChar { it.uppercase() }) },
-                icon = { Icon(Icons.Default.Home, contentDescription = null) }
+                icon = {
+                    Icon(
+                        painter = painterResource(id = iconRes),
+                        contentDescription = null,
+                        tint = if (currentRoute == route) Color(0xFF558DD8) else Color.Unspecified
+                    )
+                }
             )
         }
     }
