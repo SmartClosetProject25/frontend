93c0b8799d13a6242dc5c8cc7dcd5cba19b693ca
MatsunagaAtsuki <144500859+MatsunagaAtsuki@users.noreply.github.com>
2025-11-17 14:28:51 +0900

    GPS取得用コード
    使用する際はUI側に権限許可がいる
    現状呼び出しなし

--- app/src/main/java/com/example/smartcloset_frontend/utils/GetLocation.kt
+++ app/src/main/java/com/example/smartcloset_frontend/utils/GetLocation.kt
@@ -1,5 +1,34 @@
 package com.example.smartcloset_frontend.utils
 
-class GetLocation {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.util.Log
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
+object GetLocation {
+    //使用する際はUI側に権限許可がいる
+    @SuppressLint("MissingPermission")
+    suspend fun getLastLocationSuspend(context: Context): Location =
+        suspendCancellableCoroutine { cont ->
+            val fusedClient = LocationServices.getFusedLocationProviderClient(context)
+
+            fusedClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        cont.resume(location)
+                        Log.d("GetLocation", "getLastLocationSuspend: $location")
+                    } else {
+                        cont.resumeWithException(
+                            IllegalStateException("現在地が取得できませんでした（null）")
+                        )
+                    }
+                }
+                .addOnFailureListener { e ->
+                    cont.resumeWithException(e)
+                }
+        }
 }
