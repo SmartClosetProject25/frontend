5847f7ff23abf70aef038ad51c127f0b6bf5d417
syxnji <syunji0710@gmail.com>
2025-12-12 11:59:00 +0900

    追加 画像の表示

--- app/src/main/java/com/example/smartcloset_frontend/data/repository/TodayPlanRepository.kt
+++ app/src/main/java/com/example/smartcloset_frontend/data/repository/TodayPlanRepository.kt
@@ -1,17 +1,17 @@
 package com.example.smartcloset_frontend.data.repository
 
 import com.example.smartcloset_frontend.data.GenerateImageRequest
+import com.example.smartcloset_frontend.data.ImageResponse
 import com.example.smartcloset_frontend.data.ProposalResponse
 import com.example.smartcloset_frontend.data.TodayPlanData
 import com.example.smartcloset_frontend.network.RetrofitClient
-import okhttp3.ResponseBody
 import retrofit2.Response
 
 class TodayPlanRepository {
     suspend fun sendTodayPlan(todayPlanData: TodayPlanData): Response<ProposalResponse> =
         RetrofitClient.instance.sendTodayPlan(todayPlanData)
 
-    suspend fun generateImage(itemIds: List<String>): Response<ResponseBody> {
+    suspend fun generateImage(itemIds: List<String>): Response<ImageResponse> {
         val request = GenerateImageRequest(item_ids = itemIds)
         return RetrofitClient.instance.generateImage(request)
     }
